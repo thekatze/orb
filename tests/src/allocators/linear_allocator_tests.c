4bd6d816991189af58dda89769cc70517a6c48d9
Leo Katzengruber <leofel.katz@gmail.com>
2024-08-02 11:43:50 +0200

    add more linear allocator tests

--- tests/src/allocators/linear_allocator_tests.c
+++ tests/src/allocators/linear_allocator_tests.c
@@ -35,6 +35,52 @@ b8 linear_allocator_should_allocate_linearly() {
     return true;
 }
 
+b8 linear_allocator_should_allocate_entire_view() {
+    const usize items = 128;
+
+    u8 *memory = orb_platform_allocate(sizeof(u8) * items, false);
+    orb_linear_allocator allocator;
+    orb_linear_allocator_create_view(sizeof(u8) * items, memory, &allocator);
+
+    for (usize i = 0; i < items; ++i) {
+        u8 *block = orb_linear_allocator_allocate(&allocator, sizeof(u8));
+
+        expect_neq(0, block);
+        expect_eq(sizeof(*memory) * (i + 1), allocator.allocated);
+    }
+
+    orb_platform_free(memory, false);
+
+    return true;
+}
+
+b8 linear_allocator_should_reset() {
+    const usize items = 4;
+
+    i32 *memory = orb_platform_allocate(sizeof(i32) * items, false);
+    orb_linear_allocator allocator;
+    orb_linear_allocator_create_view(sizeof(i32) * items, memory, &allocator);
+
+    // fill allocator up
+    for (usize i = 0; i < items; ++i) {
+        orb_linear_allocator_allocate(&allocator, sizeof(i32));
+    }
+
+    orb_linear_allocator_reset(&allocator);
+
+    expect_eq(allocator.allocated, 0);
+
+    i32 *block = orb_linear_allocator_allocate(&allocator, sizeof(*memory));
+
+    expect_eq(allocator.memory, block);
+
+    orb_platform_free(memory, false);
+
+    return true;
+}
+
 void linear_allocator_register_tests() {
     test_runner_register(linear_allocator_should_allocate_linearly);
+    test_runner_register(linear_allocator_should_allocate_entire_view);
+    test_runner_register(linear_allocator_should_reset);
 }
