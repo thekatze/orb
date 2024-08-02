0ace67ec365817c0c213ad49d29b83aae8e60a88
Leo Katzengruber <leofel.katz@gmail.com>
2024-08-02 16:50:20 +0200

    quit example when pressing escape

--- example/src/game.c
+++ example/src/game.c
@@ -1,25 +1,33 @@
 #include "game.h"
-#include "orb/core/event.h"
-#include "orb/core/input.h"
-#include "orb/core/logger.h"
-#include "orb/core/orb_memory.h"
-#include "orb/core/orb_string.h"
-#include "orb/math/random.h"
-#include "orb/platform/platform.h"
+
+#include <orb/core/event.h>
+#include <orb/core/input.h>
+#include <orb/core/logger.h>
+#include <orb/core/orb_memory.h>
+#include <orb/core/orb_string.h>
+#include <orb/math/random.h>
+#include <orb/platform/platform.h>
 
 b8 handle_key_press(u16 code, void *source, void *listener, orb_event_context context) {
     (void)code;
     (void)listener;
     (void)source;
     switch (context.data.u16[0]) {
-    case KEY_A:
+    case KEY_A: {
         ORB_INFO("A PRESSED!");
         break;
-    case KEY_SPACE:
+    }
+    case KEY_SPACE: {
         char *stats = orb_memory_debug_stats();
         ORB_INFO("%s", stats);
         orb_free(stats, orb_string_length(stats) + 1, MEMORY_TAG_STRING);
         break;
+    }
+    case KEY_ESCAPE: {
+        orb_event_context quit_context = {};
+        orb_event_send(ORB_EVENT_APPLICATION_QUIT, listener, quit_context);
+        break;
+    }
     }
 
     return false;
