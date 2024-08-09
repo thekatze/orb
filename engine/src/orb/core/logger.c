6328f333e74e4b582b6c0b28945bf33955ea0ce1
Leo Katzengruber <leofel.katz@gmail.com>
2024-08-09 17:14:27 +0200

    refactor logger to use engine apis

--- engine/src/orb/core/logger.c
+++ engine/src/orb/core/logger.c
@@ -1,11 +1,10 @@
 #include "logger.h"
 #include "../platform/filesystem.h"
 #include "../platform/platform.h"
+#include "orb_memory.h"
 #include "orb_string.h"
 
 #include <stdarg.h>
-#include <stdio.h>
-#include <string.h>
 
 void orb_report_assertion_failure(const char *expression, const char *message, const char *file,
                                   i32 line, const char *function) {
@@ -27,7 +26,7 @@ b8 orb_logger_init(u64 *memory_requirement, void *memory) {
 
     state = memory;
 
-    if (!orb_filesystem_file_open("RUN_TIMESTAMP.log", FILE_MODE_WRITE_FLAG, false,
+    if (!orb_filesystem_file_open("latest.log", FILE_MODE_WRITE_FLAG, false,
                                   &state->log_file)) {
         orb_platform_console_write_error("[ERROR]: Unable to open logfile.", LOG_LEVEL_ERROR);
         return false;
@@ -49,15 +48,15 @@ void orb_log(orb_log_level level, const char *message, ...) {
     // stack allocate, logger must be high performance
     const u32 max_message_len = 32 * 1024;
     char formatted_message[max_message_len];
-    memset(formatted_message, 0, sizeof(formatted_message));
+    orb_memory_zero(formatted_message, sizeof(formatted_message));
 
     va_list arg_ptr;
     va_start(arg_ptr, message);
-    vsnprintf(formatted_message, max_message_len, message, arg_ptr);
+    orb_string_format_v(formatted_message, message, arg_ptr);
     va_end(arg_ptr);
 
     char out_message[max_message_len];
-    sprintf(out_message, "%s%s\n", level_strings[level], formatted_message);
+    orb_string_format(out_message, "%s%s\n", level_strings[level], formatted_message);
 
     usize message_length = orb_string_length(out_message);
     usize written = 0;
