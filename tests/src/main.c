07a1216de5b7b9210b82c8bfe08ed0a81dc4a3e9
Leo Katzengruber <leofel.katz@gmail.com>
2024-09-12 21:47:21 +0200

    fix test segfault

--- tests/src/main.c
+++ tests/src/main.c
@@ -7,10 +7,23 @@
 #include "math/orb_math_tests.h"
 
 i32 main() {
+    usize logger_memory_requirement;
+    orb_logger_init(&logger_memory_requirement, nullptr);
+
+    void *logger_memory = orb_platform_allocate(logger_memory_requirement, false);
+    orb_logger_init(&logger_memory_requirement, logger_memory);
+
+    ORB_DEBUG("Initializing test runner");
     test_runner_initialize();
 
     orb_math_register_tests();
     linear_allocator_register_tests();
 
-    return test_runner_run() == 0 ? 0 : 1;
+    ORB_DEBUG("Starting test run");
+    bool result = test_runner_run() == 0 ? 0 : 1;
+
+    orb_logger_shutdown();
+    orb_platform_free(logger_memory, false);
+
+    return result;
 }
